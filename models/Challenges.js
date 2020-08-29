de352e8e51962bdd9772a53f2060337018c82fd5
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-08-29 08:02:54 -0500

    add models challeges and solutions

---
+++ models/Challenges.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ChallengeSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "Users"
+    },
+    challengeInfo: {
+      title: { type: String, maxlength: 280, require: true },
+      description: { type: String, maxlength: 280, require: true },
+      tips:{type: Array },
+      url: { type: String, require: true },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Challenge = mongoose.model('Challenges', ChallengeSchema);
+
+module.exports = Challenge;
